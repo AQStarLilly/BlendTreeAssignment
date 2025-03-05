96a679731e9f1e460001a55428c9dfc0757efe2f
Kieran Hislop <kieranjhislop@gmail.com>
2025-03-05 14:38:05 -0400

    Player animations now play while moving and stop with you

--- Assets/CharacterController/PlayerMovement.cs
+++ Assets/CharacterController/PlayerMovement.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public Animator animator;
     private Rigidbody2D playerRigidbody;
     public Vector2 moveVector = Vector2.zero;
 
@@ -16,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     private float sprintMultiplier = 2f;
 
     private float currentSpeed;
+    public float magnitudeDebug;
 
     // Start is called before the first frame update
     void Awake()
@@ -31,6 +34,10 @@ public class PlayerMovement : MonoBehaviour
         playerRigidbody = this.GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        HandleAnimation();
+    }
     private void UpdateMoveVector(Vector2 InputVector)
     {
         moveVector = InputVector;
@@ -49,6 +56,23 @@ public class PlayerMovement : MonoBehaviour
     void HandlePlayerMovement()
     {
         playerRigidbody.MovePosition(playerRigidbody.position + moveVector * currentSpeed * Time.fixedDeltaTime);
+    }
+
+    void HandleAnimation()
+    {
+        magnitudeDebug = moveVector.magnitude;
+               
+
+        if(moveVector.magnitude != 0)
+        {
+            animator.SetFloat("Horizontal", moveVector.x);
+            animator.SetFloat("Vertical", moveVector.y);
+            animator.SetBool("Moving", true);
+        }
+        else
+        {          
+            animator.SetBool("Moving", false);
+        }
     }
 
     void OnDisable()
